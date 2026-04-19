fb1dc7b584c497f2e07c3b70989337f6f07fb3d4
Gar_Max <maksimum585@gmail.com>
2026-04-19 20:38:25 +0300

    Second commit

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -21,7 +21,7 @@ ymaps.ready(init);
             });
         }
 
-        // Пункты прибытия (оранжевые)
+        // Пункты прибытия (зелёные)
         const arrivals = [
             {coords:[43.222,76.851], title:'Казахская ССР (Алма-Ата)', file:'templates/kazakhstan.html'},
             {coords:[41.311,69.279], title:'Узбекская ССР (Ташкент)', file:'templates/uzbekistan.html'},
@@ -40,37 +40,31 @@ ymaps.ready(init);
         ];
 
         arrivals.forEach(a => {
-            const placemark = createPlacemark(a.coords, a.title, a.file, '#d47b28');
+            const placemark = createPlacemark(a.coords, a.title, a.file, '#008000');
             map.geoObjects.add(placemark);
             placemark.events.add('click', function () {
                 placemark.balloon.open();
             });
         });
 
-        // Пункты отправления (жёлтые)
+        // Пункты отправления (красные)
         const departures = [
-            {coords:[59.939,30.315], title:'Ленинград'},
-            {coords:[55.755,37.617], title:'Москва'},
-            {coords:[50.450,30.523], title:'Киев'},
-            {coords:[53.904,27.561], title:'Минск'},
-            {coords:[46.485,30.743], title:'Одесса'},
-            {coords:[47.910,33.391], title:'Кривой Рог'},
-            {coords:[49.993,36.230], title:'Харьков'},
-            {coords:[47.838,35.140], title:'Запорожье'},
-            {coords:[48.022,37.802], title:'Донбасс (Донецк)'},
-            {coords:[56.946,24.105], title:'Рига'},
-            {coords:[59.437,24.753], title:'Таллин'},
-            {coords:[54.687,25.280], title:'Вильнюс'}
+            {coords:[59.939,30.315], title:'Ленинград', file: 'templates/leningrad.html'},
+            {coords:[55.755,37.617], title:'Москва', file: 'templates/moscow.html'},
+            {coords:[50.450,30.523], title:'Киев', file: 'templates/ukrain.html'},
+            {coords:[53.904,27.561], title:'Минск', file: 'templates/belorussia.html'},
+            {coords:[46.485,30.743], title:'Одесса', file: 'templates/ukrain.html'},
+            {coords:[47.910,33.391], title:'Кривой Рог', file: 'templates/ukrain.html'},
+            {coords:[49.993,36.230], title:'Харьков', file: 'templates/ukrain.html'},
+            {coords:[47.838,35.140], title:'Запорожье', file: 'templates/ukrain.html'},
+            {coords:[48.022,37.802], title:'Донбасс (Донецк)', file: 'templates/ukrain.html'},
+            {coords:[56.946,24.105], title:'Рига', file: 'templates/riga.html'},
+            {coords:[59.437,24.753], title:'Таллин', file: 'templates/tallin.html'},
+            {coords:[54.687,25.280], title:'Вильнюс', file: 'templates/vilnus.html'}
         ];
 
         departures.forEach(d => {
-            const placemark = new ymaps.Placemark(d.coords, {
-                balloonContent: `<b>${d.title}</b>`
-            }, {
-                preset: 'islands#circleIcon',
-                iconColor: '#e8b34b',
-                balloonMaxWidth: 300
-            });
+            const placemark = createPlacemark(d.coords, d.title, d.file, '#E31A1C');
             map.geoObjects.add(placemark);
             placemark.events.add('click', function () {
                 placemark.balloon.open();
